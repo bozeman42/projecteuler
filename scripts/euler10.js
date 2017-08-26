ee9fae0765c59c72bea9f338356c0f980e959a95
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-08-26 12:32:35 -0500

    Used const for euler10 upper limit

--- scripts/euler10.js
+++ scripts/euler10.js
@@ -5,15 +5,17 @@ Find the sum of all the primes below two million.
 
 */
 
+const UPPER_LIMIT = 2000000;
+
 function main() {
     var t0 = performance.now();
     var numArray = [];
     // Build list of numbers;
-    for (var i = 3; i < 2000000; i += 2) {
+    for (var i = 3; i < UPPER_LIMIT; i += 2) {
         numArray.push(i);
     }
     var sum = 2;
-    for (var j = 3; j < Math.sqrt(2000000); j = numArray[0]) {
+    for (var j = 3; j < Math.sqrt(UPPER_LIMIT); j = numArray[0]) {
         sum += j;
         numArray = filterPrime(numArray,j);
     }
